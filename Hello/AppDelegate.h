11495b39dfc6cf081c679323d00211de0629631e
xierongping <956893898@qq.com>
2017-05-19 09:45:09 +0800

    Initial Commit

---
+++ Hello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hello
+//
+//  Created by cier on 17/5/19.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
