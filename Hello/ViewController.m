b1299051e7a8b4768943c63dcd1d9ba6c4ee0027
xierongping <956893898@qq.com>
2017-05-19 10:21:21 +0800

    first commet

--- Hello/ViewController.m
+++ Hello/ViewController.m
@@ -17,6 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(100, 100, 200, 100)];
+    label.text = @"hello world cier";
+    label.font = [UIFont systemFontOfSize:22];
+    label.textAlignment = NSTextAlignmentCenter;
+    label.textColor= [UIColor cyanColor];
+    label.backgroundColor= [UIColor redColor];
+    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning {
